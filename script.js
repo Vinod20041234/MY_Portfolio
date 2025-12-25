5bbd56907d9fb27af2c983dde31ff9a6ad9893e1
Vinod V <149381788+Vinod20041234@users.noreply.github.com>
2025-12-25 22:42:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,24 +3,31 @@ const commandInput = document.getElementById("command");
 
 const responses = {
   help: `Commands:
-  resume: `
-<b>Resume</b><br>
-<a href="Vinod_Resume.pdf" download style="color:#38bdf8;">
-👉 Click here to download my resume
-</a>
-`,
  - about
 - education
 - skills
 - projects
 - resume
+-certifications
 - github
 - linkedin`,
   about: "Vinod Vishwanath | Software Engineer | AI/ML | Backend Developer",
   education: "B.E. ECE | Shridevi Institute of Engineering & Technology | CGPA: 7.55",
   skills: "Python, C, React, Node.js, MongoDB, PostgreSQL, AI/ML",
   projects: "Mini: Coal Mining Hazard Detection (IoT) | Major: Heart Disease Detection (ML)",
-  resume: "Email: vinodvishwanathshetty@gmail.com | Phone: +91 7619564865",
+  resume: `
+<b>Resume</b><br>
+<a href="Vinod_Resume.pdf" download style="color:#38bdf8;">
+👉 Click here to download my resume
+</a>
+`,
+  certifications: `
+<b>Certifications</b><br><br>
+* Artificial Intelligence Fundamentals – IBM SkillBuild<br>
+* AI (19 Courses) – Infosys Springboard<br>
+* Python for Data Science – Udemy<br>
+* Agentic AI – Google Cloud
+  `,
   github: "https://github.com/Vinod20041234",
   linkedin: "https://www.linkedin.com/in/vinod-v-a6a437289"
 };
